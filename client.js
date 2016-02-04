feef020fb8169095393eaa7ed33493304e18b224
aequanimitas <htavenido@gmail.com>
2016-02-05 00:36:43 +0800

    testing

--- client.js
+++ client.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import ol from 'openlayers';
+
+const placeLayer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: './OSGEoLabs.json'
+  })
+})
+
+const map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    }),
+    placeLayer
+  ],
+  view: new ol.View({
+    center: [949282, 6002552],
+    zoom: 4
+  })
+});
+
 class App extends Component {
   render() {
     return <div>Test!!!</div>
